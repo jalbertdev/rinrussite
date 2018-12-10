b7e10e5f1f4ba3041112a872f150bd2873e75cdc
Josh Albert <jalbert@memphis.edu>
2018-12-10 16:02:33 -0600

    Added upload feature and error checking.

--- rinrusmain/models.py
+++ rinrusmain/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Simulation(models.Model):
+	unProcessedPDB = models.CharField(max_length=100)
+	idNumber=models.IntegerField(default=0)
+	valid=models.BooleanField() #add default to false
+	dateRan=models.DateTimeField()
+	simValues=models.CharField(max_length=100)
+	simName=models.CharField(max_length=100)
+	
--- rinrusmain/views.py
+++ rinrusmain/views.py
@@ -5,8 +5,24 @@ from django.shortcuts import get_object_or_404, render
 #from .models import Choice, Question
 from django.urls import reverse
 from django.utils import timezone
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
 
 
 def index(request):
-	return render(request,'rinrusmain/index.html')
+        if request.method == 'POST' and request.FILES['myfile']:
+                myfile = request.FILES['myfile']
+                fs = FileSystemStorage()
+                if myfile.content_type == 'application/octet-stream':
+                        fileName=request.POST["jobName"]+"'"+request.POST["row1"]+".pdb"
+                        filename = fs.save(fileName, myfile)
+                        uploaded_file_url = fs.url(filename)
+                        return render(request, 'rinrusmain/index.html', {'uploaded_file_url': uploaded_file_url})
+                else:
+                        return render(request, 'rinrusmain/index.html',{'uploaded_file_error': True})
+        return render(request,'rinrusmain/index.html')
+	
+def about(request):
+        return render(request,'rinrusmain/about.html')
+
 
