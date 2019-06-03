096f7e3dcdd54f3acdc2b9a8546d1bdceaf96fb7
jalbertdev <thebackpacker101@gmail.com>
2019-06-03 16:39:12 -0500

    Improved main script

--- rinrusmain/scripts/processing_script.py
+++ rinrusmain/scripts/processing_script.py
@@ -1,11 +1,12 @@
 import os, shutil
 #import rinrus_algs.probe2rins as p2r
 
-def runScripts(path):
+def run_scripts(path, residues, chains,name): #path in the format starting at the static folder
     os.system("chmod u+x rinrus_algs/probe")
     probe_string="./rinrus_algs/probe "
     probe_string+=path
-    probe_path="" #create probe file path
+    
+    probe_path=(path[:path.rfind('/')])+name+".probe" #make probe file path
     probe_string+=" > "
     probe_string+=probe_path
     os.system(probe_string)
@@ -17,7 +18,10 @@ def runScripts(path):
 
     #probe_freq_2pdb.py: python ../scripts/probe_freq_2pdb.py 3bwm_h_mg_ts_wGlu199.ent file.probe file.probe A,300,A,301,A,304
 
-    #pymol_scripts.py 
+    pymol -qc log.pymol
+
+    for files:
+        #python pymol_scripts.py filename 301,302
 
     shutil.make_archive(outputpath, 'zip', folderpath)
 
