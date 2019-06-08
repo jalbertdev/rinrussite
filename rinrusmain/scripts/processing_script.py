7fa2f7df55a2102eb89a99a66ca502f92d2d67d8
jalbertdev <thebackpacker101@gmail.com>
2019-06-08 17:25:16 -0500

    Fleshed out the processing script more

--- rinrusmain/scripts/processing_script.py
+++ rinrusmain/scripts/processing_script.py
@@ -1,29 +1,55 @@
 import os, shutil
 #import rinrus_algs.probe2rins as p2r
 
-def run_scripts(path, residues, chains,name): #path in the format starting at the static folder
+def get_res_chain(residues,chains):
+    length=min(len(residues),len(chains))
+    res=""
+    for x in range(length):
+        res+=chains[x]
+        res+=":"
+        res+=residues[x]
+        res+=","
+    res=res[:len(res)-1]
+    return res
+
+def run_scripts(path, residues, chains, name): #path in the format starting at the static folder
+    print(os.getcwd())
+    reschain=get_res_chain(residues,chains)
+    path="" #create PDB path
+
+    #Step 1
     os.system("chmod u+x rinrus_algs/probe")
-    probe_string='./rinrus_algs/probe -unformated -self "all"'
+    probe_string='./rinrus_algs/probe -unformated -self "all" '
     probe_string+=path
-    
     probe_path=(path[:path.rfind('/')])+name+".probe" #make probe file path
-    probe_string+=" > "
-    probe_string+=probe_path
+    probe_string+=" > " + probe_path
     os.system(probe_string)
 
+    #Step 2
     probe2rins_string="python rinrusAlgs/probe2rins.py "
     probe2rins_string+=probe_path
-    probe2rins_string+=" A:300,A:301,A:302"
+    probe2rins_string+=reschain
     os.system(probe2rins_string)
 
-    #probe_freq_2pdb.py: python ../scripts/probe_freq_2pdb.py 3bwm_h_mg_ts_wGlu199.ent file.probe file.probe A,300,A,301,A,304
+    #Step 3
+    #./../scripts/probe_freq_2pdb.py 3bwm_h_mg_ts_wGlu199.ent file.probe freq_per_res.dat  A,300,A,301,A,302
+    probe_freq_string="python rinrusAlgs/probe_freq_2pdb.py " + path + " " + probe_path + " "
+    freq_path=""
+    probe_freq_string+= freq_path + " " + reschain
+    os.system(probe_freq_string)
 
-    pymol -qc log.pymol
+    #Step 4
+    log_path=""
+    os.system("pymol -qc "+log_path)
 
-    for files:
+    #Step 5
+    #for files:
         #python pymol_scripts.py filename 301,302
 
-    shutil.make_archive(outputpath, 'zip', folderpath)
+    #Step 6 Create Zip File
+    folder_path=path[:path.rfind('/')]
+    output_path=output_path[:output_path.rfind('/')]+name+".zip"
+    shutil.make_archive(output_path, 'zip', folder_path)
 
 
 
